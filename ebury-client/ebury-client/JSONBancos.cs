5ff5ea2c8474aac5ce1cdf47a8fbd773ccc53414
GitHubber-01 <73506900+GitHubber-01@users.noreply.github.com>
2021-12-13 11:51:38 +0100

    Clases adicionales
    FIltro bancos

---
+++ ebury-client/ebury-client/JSONBancos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ebury_client
+{
+    class JSONBancos
+    {
+        public string accountNumber, balance, accountState, accountType;
+        public List<Particular> particulares;
+        
+        public JSONBancos(string accountNumber, string balance, string accountState, string accountType)
+        {
+            this.accountNumber = accountNumber;
+            this.balance = balance;
+            this.accountState = accountState;
+            this.accountType = accountType;
+            particulares = new List<Particular>();
+        }
+        public override string ToString()
+        {
+            return "(" + accountNumber + ", " + balance + ", " + accountState + ", " + accountType + ", [" +
+                string.Join<Particular>(", ", particulares) + "])";
+        }
+    }
+}
---
+++ ebury-client/ebury-client/Particular.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ebury_client
+{
+    class Particular
+    {
+        public string firstName, lastName, birthDate, city, street, number, postalCode, country, nif;
+
+        public Particular(string firstName, string lastName, string birthDate, string city,
+            string street, string number, string postalCode, string country, string nif)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.birthDate = birthDate.Substring(0, 10);
+            this.city = city;
+            this.street = street;
+            this.number = number;
+            this.postalCode = postalCode;
+            this.country = country;
+            this.nif = nif;
+        }
+
+        public override string ToString()
+        {
+            return "(" + firstName + ", " + lastName + ", " + birthDate + ", " + city + ", " + street + ", " +
+                number + ", " + postalCode + ", " + country + ", " + nif + ")";
+        }
+    }
+}
