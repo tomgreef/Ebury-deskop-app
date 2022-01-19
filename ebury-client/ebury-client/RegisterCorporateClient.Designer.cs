adde8fb1bb985c8eff220919dd3db600972839c9
Tom van Greevenbroek <33311803+tomgreef@users.noreply.github.com>
2022-01-19 13:34:55 +0100

    Diseño - Regristro y Gestión Empresas

--- ebury-client/ebury-client/RegisterCorporateClient.Designer.cs
+++ ebury-client/ebury-client/RegisterCorporateClient.Designer.cs
@@ -28,11 +28,374 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tPrimerNombre = new System.Windows.Forms.TextBox();
+            this.tNIF = new System.Windows.Forms.TextBox();
+            this.tNumero = new System.Windows.Forms.TextBox();
+            this.tPuerta = new System.Windows.Forms.TextBox();
+            this.tCalle = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.tCP = new System.Windows.Forms.TextBox();
+            this.tRegion = new System.Windows.Forms.TextBox();
+            this.tPais = new System.Windows.Forms.TextBox();
+            this.tCiudad = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.cValida = new System.Windows.Forms.CheckBox();
+            this.bRegistrar = new System.Windows.Forms.Button();
+            this.bCancelar = new System.Windows.Forms.Button();
+            this.tRepetirContrasena = new System.Windows.Forms.TextBox();
+            this.tContrasena = new System.Windows.Forms.TextBox();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(154, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REGISTRO DE EMPRESA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 44);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Datos de la empresa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 161);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dirección";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 80);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CIF (*)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(20, 112);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(126, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Nombre de la empresa (*)";
+            // 
+            // tPrimerNombre
+            // 
+            this.tPrimerNombre.Location = new System.Drawing.Point(150, 109);
+            this.tPrimerNombre.Margin = new System.Windows.Forms.Padding(2);
+            this.tPrimerNombre.Name = "tPrimerNombre";
+            this.tPrimerNombre.Size = new System.Drawing.Size(380, 20);
+            this.tPrimerNombre.TabIndex = 9;
+            // 
+            // tNIF
+            // 
+            this.tNIF.Location = new System.Drawing.Point(61, 77);
+            this.tNIF.Margin = new System.Windows.Forms.Padding(2);
+            this.tNIF.Name = "tNIF";
+            this.tNIF.Size = new System.Drawing.Size(175, 20);
+            this.tNIF.TabIndex = 14;
+            // 
+            // tNumero
+            // 
+            this.tNumero.Location = new System.Drawing.Point(333, 189);
+            this.tNumero.Margin = new System.Windows.Forms.Padding(2);
+            this.tNumero.Name = "tNumero";
+            this.tNumero.Size = new System.Drawing.Size(198, 20);
+            this.tNumero.TabIndex = 21;
+            // 
+            // tPuerta
+            // 
+            this.tPuerta.Location = new System.Drawing.Point(158, 218);
+            this.tPuerta.Margin = new System.Windows.Forms.Padding(2);
+            this.tPuerta.Name = "tPuerta";
+            this.tPuerta.Size = new System.Drawing.Size(109, 20);
+            this.tPuerta.TabIndex = 20;
+            // 
+            // tCalle
+            // 
+            this.tCalle.Location = new System.Drawing.Point(78, 188);
+            this.tCalle.Margin = new System.Windows.Forms.Padding(2);
+            this.tCalle.Name = "tCalle";
+            this.tCalle.Size = new System.Drawing.Size(188, 20);
+            this.tCalle.TabIndex = 19;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(20, 190);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(43, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Calle (*)";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(20, 222);
+            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(136, 13);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Planta / Puerta / Oficina (*)";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(285, 192);
+            this.label12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(44, 13);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "Número";
+            // 
+            // tCP
+            // 
+            this.tCP.Location = new System.Drawing.Point(333, 278);
+            this.tCP.Margin = new System.Windows.Forms.Padding(2);
+            this.tCP.Name = "tCP";
+            this.tCP.Size = new System.Drawing.Size(198, 20);
+            this.tCP.TabIndex = 29;
+            // 
+            // tRegion
+            // 
+            this.tRegion.Location = new System.Drawing.Point(333, 249);
+            this.tRegion.Margin = new System.Windows.Forms.Padding(2);
+            this.tRegion.Name = "tRegion";
+            this.tRegion.Size = new System.Drawing.Size(198, 20);
+            this.tRegion.TabIndex = 28;
+            // 
+            // tPais
+            // 
+            this.tPais.Location = new System.Drawing.Point(78, 276);
+            this.tPais.Margin = new System.Windows.Forms.Padding(2);
+            this.tPais.Name = "tPais";
+            this.tPais.Size = new System.Drawing.Size(188, 20);
+            this.tPais.TabIndex = 27;
+            // 
+            // tCiudad
+            // 
+            this.tCiudad.Location = new System.Drawing.Point(78, 247);
+            this.tCiudad.Margin = new System.Windows.Forms.Padding(2);
+            this.tCiudad.Name = "tCiudad";
+            this.tCiudad.Size = new System.Drawing.Size(188, 20);
+            this.tCiudad.TabIndex = 26;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(20, 249);
+            this.label13.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(53, 13);
+            this.label13.TabIndex = 25;
+            this.label13.Text = "Ciudad (*)";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(20, 279);
+            this.label14.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(42, 13);
+            this.label14.TabIndex = 24;
+            this.label14.Text = "País (*)";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(285, 251);
+            this.label15.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(41, 13);
+            this.label15.TabIndex = 23;
+            this.label15.Text = "Región";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(285, 280);
+            this.label16.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(40, 13);
+            this.label16.TabIndex = 22;
+            this.label16.Text = "C.P. (*)";
+            // 
+            // cValida
+            // 
+            this.cValida.AutoSize = true;
+            this.cValida.Location = new System.Drawing.Point(61, 312);
+            this.cValida.Margin = new System.Windows.Forms.Padding(2);
+            this.cValida.Name = "cValida";
+            this.cValida.Size = new System.Drawing.Size(141, 17);
+            this.cValida.TabIndex = 30;
+            this.cValida.Text = "Válida (Dirección actual)";
+            this.cValida.UseVisualStyleBackColor = true;
+            // 
+            // bRegistrar
+            // 
+            this.bRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bRegistrar.Location = new System.Drawing.Point(61, 414);
+            this.bRegistrar.Margin = new System.Windows.Forms.Padding(2);
+            this.bRegistrar.Name = "bRegistrar";
+            this.bRegistrar.Size = new System.Drawing.Size(74, 26);
+            this.bRegistrar.TabIndex = 31;
+            this.bRegistrar.Text = "Registrar";
+            this.bRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // bCancelar
+            // 
+            this.bCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCancelar.Location = new System.Drawing.Point(397, 414);
+            this.bCancelar.Margin = new System.Windows.Forms.Padding(2);
+            this.bCancelar.Name = "bCancelar";
+            this.bCancelar.Size = new System.Drawing.Size(74, 26);
+            this.bCancelar.TabIndex = 32;
+            this.bCancelar.Text = "Cancelar";
+            this.bCancelar.UseVisualStyleBackColor = true;
+            this.bCancelar.Click += new System.EventHandler(this.bCancelar_Click);
+            // 
+            // tRepetirContrasena
+            // 
+            this.tRepetirContrasena.Location = new System.Drawing.Point(370, 359);
+            this.tRepetirContrasena.Margin = new System.Windows.Forms.Padding(2);
+            this.tRepetirContrasena.Name = "tRepetirContrasena";
+            this.tRepetirContrasena.Size = new System.Drawing.Size(160, 20);
+            this.tRepetirContrasena.TabIndex = 36;
+            // 
+            // tContrasena
+            // 
+            this.tContrasena.Location = new System.Drawing.Point(100, 359);
+            this.tContrasena.Margin = new System.Windows.Forms.Padding(2);
+            this.tContrasena.Name = "tContrasena";
+            this.tContrasena.Size = new System.Drawing.Size(150, 20);
+            this.tContrasena.TabIndex = 35;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(20, 359);
+            this.label17.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(74, 13);
+            this.label17.TabIndex = 34;
+            this.label17.Text = "Contraseña (*)";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(254, 360);
+            this.label18.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(111, 13);
+            this.label18.TabIndex = 33;
+            this.label18.Text = "Repetir Contraseña (*)";
+            // 
+            // RegisterCorporateClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "RegisterCorporateClient";
+            this.ClientSize = new System.Drawing.Size(550, 465);
+            this.Controls.Add(this.tRepetirContrasena);
+            this.Controls.Add(this.tContrasena);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.bCancelar);
+            this.Controls.Add(this.bRegistrar);
+            this.Controls.Add(this.cValida);
+            this.Controls.Add(this.tCP);
+            this.Controls.Add(this.tRegion);
+            this.Controls.Add(this.tPais);
+            this.Controls.Add(this.tCiudad);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.tNumero);
+            this.Controls.Add(this.tPuerta);
+            this.Controls.Add(this.tCalle);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.tNIF);
+            this.Controls.Add(this.tPrimerNombre);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximumSize = new System.Drawing.Size(566, 800);
+            this.MinimumSize = new System.Drawing.Size(566, 495);
+            this.Name = "RegisterCorporateClient";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ebury Client App - Registro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tPrimerNombre;
+        private System.Windows.Forms.TextBox tNIF;
+        private System.Windows.Forms.TextBox tNumero;
+        private System.Windows.Forms.TextBox tPuerta;
+        private System.Windows.Forms.TextBox tCalle;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox tCP;
+        private System.Windows.Forms.TextBox tRegion;
+        private System.Windows.Forms.TextBox tPais;
+        private System.Windows.Forms.TextBox tCiudad;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.CheckBox cValida;
+        private System.Windows.Forms.Button bRegistrar;
+        private System.Windows.Forms.Button bCancelar;
+        private System.Windows.Forms.TextBox tRepetirContrasena;
+        private System.Windows.Forms.TextBox tContrasena;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
     }
 }
